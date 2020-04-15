d7f606357f28fb2614babdec5e266cb7b3709e49
tjclawson <tjamesclawson@gmail.com>
2020-04-15 19:41:30 -0400

    Set WeatherFragment to launch when app starts

--- app/src/main/java/com/tjclawson/retrofit_testing/MainActivity.kt
+++ app/src/main/java/com/tjclawson/retrofit_testing/MainActivity.kt
@@ -2,11 +2,19 @@ package com.tjclawson.retrofit_testing
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tjclawson.retrofit_testing.ui.WeatherFragment
 
 class MainActivity : AppCompatActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.main_container, WeatherFragment())
+            .commit()
     }
 }
