09f55e69069a9f53e77b980c9d0646a666389045
tjclawson <tjamesclawson@gmail.com>
2020-04-15 20:37:40 -0400

    Add logic to fetch current weather from API for location user enters

--- app/src/main/java/com/tjclawson/retrofit_testing/ui/WeatherFragment.kt
+++ app/src/main/java/com/tjclawson/retrofit_testing/ui/WeatherFragment.kt
@@ -6,8 +6,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 
 import com.tjclawson.retrofit_testing.R
+import kotlinx.android.synthetic.main.weather_fragment.*
 
 class WeatherFragment : Fragment() {
 
@@ -24,10 +28,27 @@ class WeatherFragment : Fragment() {
         return inflater.inflate(R.layout.weather_fragment, container, false)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(WeatherViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this).get(WeatherViewModel::class.java)
+        initUi()
+        initButtonListener()
+    }
+
+    private fun initUi() {
+        viewModel.currentWeather.observe(viewLifecycleOwner, Observer {
+            if (it != null)
+                text_view.text = it.currentWeather.toString()
+        })
+    }
+
+    private fun initButtonListener() {
+        button_get_weather.setOnClickListener {
+            if (et_location.text.toString().isNotEmpty())
+                viewModel.getWeather(et_location.text.toString())
+            else
+                Toast.makeText(activity, "Please Enter Location", Toast.LENGTH_LONG).show()
+        }
     }
 
 }
--- app/src/main/java/com/tjclawson/retrofit_testing/ui/WeatherViewModel.kt
+++ app/src/main/java/com/tjclawson/retrofit_testing/ui/WeatherViewModel.kt
@@ -1,7 +1,20 @@
 package com.tjclawson.retrofit_testing.ui
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.tjclawson.retrofit_testing.data.CurrentWeatherResponse
+import com.tjclawson.retrofit_testing.network.WeatherstackApiService
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class WeatherViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    private var weatherstackApiService = WeatherstackApiService()
+    val currentWeather = MutableLiveData<CurrentWeatherResponse>()
+
+    fun getWeather(location: String) {
+        GlobalScope.launch {
+            currentWeather.postValue(weatherstackApiService.getCurrentWeather(location))
+        }
+    }
 }
